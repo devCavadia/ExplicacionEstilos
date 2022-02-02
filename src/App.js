43313472f1ce195a8ac33c65dd25e376fc3bcb97
devCavadia <dcavadia93@gmail.com>
2022-02-02 16:03:40 -0500

    Agregar ejemplo de estilos con bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className='fondoNegro'>Hola mundo</span>
+      <button type="button" className="btn btn-primary">Primary</button>
+      <button type="button" className="btn btn-success">Success</button>
+      <div className="alert alert-primary" role="alert">
+        A simple primary alert—check it out!
+      </div>
+      <div class="card">
+        <img src="..." class="card-img-top" alt="..."/>
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
     </div>
   );
 }
