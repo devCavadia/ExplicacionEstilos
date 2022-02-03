04cbae450dad3fa68344ecfadc9796532134f3c3
devCavadia <dcavadia93@gmail.com>
2022-02-03 16:11:20 -0500

    Agregar explicacion de formularios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type="email"/>
+        <input type="file" />
+        <input type="number" max={10} />
+        <input type='password' />
+        <input type='text' required/>
+        <select required>
+          <option disabled value={0}>Seleccione una opcion</option>
+          <option>op2</option>
+          <option>op3</option>
+        </select>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
